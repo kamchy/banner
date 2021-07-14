866b2ee8b5b8524346c6078c85d28d21a6bc508d
Kamila Chyla <kamila.chyla@gmail.com>
2021-07-14 14:41:33 +0200

    Use simple templating in readmegenerator.go

---
+++ cmd/readmegenerator/main.go
@@ -0,0 +1,73 @@
+// blabla
+package main
+
+import (
+	"bytes"
+	"html/template"
+)
+
+type Repo struct {
+	Name  string
+	Url   string
+	Usage string
+}
+
+func generateReadme(r Repo, t *template.Template) string {
+	var b bytes.Buffer
+	t.Execute(&b, r)
+	return b.String()
+}
+
+func main() {
+	t, err := getTemplate()
+	if err != nil {
+		panic(err)
+	}
+	s := generateReadme(
+		Repo{"banner", "https://github.com/kamchy/banner", generateHelpMessage()}, t)
+	println(s)
+
+}
+
+func getTemplate() (*template.Template, error) {
+	var tmpl, err = template.New("test").Parse(`
+# Project
+{{.Name}} is a simple raster graphics generator that generates randomly a background and two lines of text.
+
+## Warning
+[{{.Name}}]({{.Url}}) is my first Go repository cretaed for language learning and is not ready for production.
+
+## Usage
+The usage options are as follows:
+` + "```" +
+		`bash
+{{.Usage}}
+` + "```" +
+		`
+## Images
+And here are images:
+`)
+
+	return tmpl, err
+}
+
+func generateHelpMessage() string {
+	return "Sample usage"
+}
+
+//func generateHelpMessage() string {
+//ifs, _ := InputFlagSet()
+//var b bytes.Buffer
+//ifs.SetOutput(&b)
+//ifs.Usage()
+//return b.String()
+//}
+//func main() {
+//if err != nil {
+//panic(err)
+//}
+//err = tmpl.Execute(os.Stdout, sweaters)
+//if err != nil {
+//panic(err)
+//}
+//}
